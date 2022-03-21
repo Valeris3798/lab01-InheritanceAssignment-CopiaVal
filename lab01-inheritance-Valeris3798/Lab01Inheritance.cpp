8db757bb6e9ee6bd938aac1f31330f65af695673
jeaus <jeaustin.rdz@gmail.com>
2022-03-21 16:43:44 -0600

    Actualizacion

--- lab01-inheritance-Valeris3798/Lab01Inheritance.cpp
+++ lab01-inheritance-Valeris3798/Lab01Inheritance.cpp
@@ -1,29 +1,41 @@
 #include <iostream>  // allows program to output data to the screen
 
-struct Base {//
-    virtual void do_something() {}
+struct Base {
+    /*
+     * //Esta NO es una buen metodo de una clase base pues no solo se declara,
+     * tambien se programa aunque sea vacio.
+     * virtual void do_something(){}
+     */
+    /*
+     * Esta SI es un buen metodo de una clase base pues es un metodo
+     * abstracto que hace que sea unicamente heredado y regla para
+     * las clases hijas
+     * virtual void do_something() =0;
+     * */
+    virtual void do_something() =0;
 };
 
-struct Derived1 : Base {
-    virtual void do_semothing() {
+struct Derived1 :  Base {
+public:
+    virtual void do_something() {
         std::cout << "Derived1!!!" << std::endl;
     }
 };
 
-struct Derived2 : Base {
-    virtual void do_semothing() {
+struct Derived2 :  Base {
+    virtual void do_something() {
         std::cout << "Derived2!!!" << std::endl;
     }
 };
 
 // function main begins program execution
-int main(int argc, const char *argv[]) {
+int main() {
     std::cout << "Welcome to the UNA!" << std::endl;
 
     Derived1 derived1;
-    derived1.do_semothing();
+    derived1.do_something();
 
     Derived2 derived2;
-    derived2.do_semothing();
+    derived2.do_something();
 
 }  // end function main
